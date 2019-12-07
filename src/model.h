2e357746f99406ed6a4d01eb56af4654feaefc76
Richard Hauerland <richlandfx@gmail.com>
2019-12-07 20:03:31 +0100

    Added new model loader with UV mapping support

--- src/model.h
+++ src/model.h
@@ -115,4 +115,178 @@ public:
     }
 };
 
+class ModelUV
+{
+private:
+    vector<glm::vec3> positions;
+    vector<glm::vec3> normals;
+    vector<unsigned int> indices;
+    vector<glm::vec2> uv;
+    unsigned indexCount;
+
+    unsigned int VAO = 0;
+    unsigned int VBO;
+    unsigned int EBO;
+
+    bool loadFlag = false;
+    bool initFlag = false;
+
+    string pathPositions;
+    string pathNormals;
+    string pathIndices;
+    string pathUV;
+
+    void load()
+    {
+        ifstream fin;
+        fin.open(pathPositions, ios::in);
+
+        if (fin)
+        {
+            while (true)
+            {
+                glm::vec3 value;
+                fin >> value.x >> value.y >> value.z;
+
+                if (fin.eof())
+                    break;
+
+                positions.push_back(value);
+            }
+
+            fin.close();
+        }
+
+        fin.open(pathNormals, ios::in);
+
+        if (fin)
+        {
+            while (true)
+            {
+                glm::vec3 value;
+                fin >> value.x >> value.y >> value.z;
+
+                if (fin.eof())
+                    break;
+
+                normals.push_back(value);
+            }
+
+            fin.close();
+        }
+
+        fin.open(pathIndices, ios::in);
+
+        if (fin)
+        {
+            while (true)
+            {
+                unsigned int value;
+                fin >> value;
+
+                if (fin.eof())
+                    break;
+
+                indices.push_back(value);
+            }
+
+            fin.close();
+        }
+
+        fin.open(pathUV, ios::in);
+
+        if (fin)
+        {
+            while (true)
+            {
+                glm::vec2 value;
+                fin >> value.x >> value.y;
+
+                if (fin.eof())
+                    break;
+
+                uv.push_back(value);
+            }
+
+            fin.close();
+        }
+
+        indexCount = indices.size();
+
+        if (positions.size() && normals.size() && uv.size() && indices.size())
+            loadFlag = true;
+        else
+            return;
+    }
+
+    void init()
+    {
+        if (loadFlag == false)
+            return;
+
+        glGenVertexArrays(1, &VAO);
+
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+
+        vector<float> data;
+
+        for (unsigned i = 0; i < positions.size(); ++i)
+        {
+            data.push_back(positions[i].x);
+            data.push_back(positions[i].y);
+            data.push_back(positions[i].z);
+
+            if (uv.size() > 0)
+            {
+                data.push_back(uv[i].x);
+                data.push_back(uv[i].y);
+            }
+
+            if (normals.size() > 0)
+            {
+                data.push_back(normals[i].x);
+                data.push_back(normals[i].y);
+                data.push_back(normals[i].z);
+            }
+        }
+
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, data.size() * sizeof(float), &data[0], GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+        float stride = (3 + 2 + 3) * sizeof(float);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride, (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(2);
+        glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, stride, (void*)(5 * sizeof(float)));
+
+        initFlag = true;
+    }
+
+public:
+    ModelUV(string _pathPositions, string _pathNormals, string _pathIndices, string _pathUV)
+    {
+        pathPositions = _pathPositions;
+        pathNormals = _pathNormals;
+        pathIndices = _pathIndices;
+        pathUV = _pathUV;
+
+        load();
+        init();
+    }
+
+    void render()
+    {
+        if (initFlag)
+        {
+            glBindVertexArray(VAO);
+            glDrawElements(GL_TRIANGLE_STRIP, indexCount, GL_UNSIGNED_INT, 0);
+        }
+    }
+};
+
 #endif
